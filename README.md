ea0f3dd92e88a886ea785b430b6fc7a7baf19245
soloistwang <342916053@qq.com>
2019-11-17 01:02:12 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# todo-app
+
+> A Vue.js todolist project
+
+## Build Setup
+
+``` bash
+npm install
+
+# 开发环境中运行 localhost:8080
+npm run dev
+
+# 创建生产环境代码
+npm run build
+
+
+# 运行单元测试
+npm run unit
+
+```
+
+reference: https://github.com/scotch-io/vue-todo
---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <h1 class="ui dividing centered header">Vue2 Todolist App</h1>
+    <div class='ui three column centered grid'>
+      <div class='column'>
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sweetalert from 'sweetalert';
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo 1',
+        project: 'Project 1',
+        done: false,
+      }, {
+        title: 'Todo 2',
+        project: 'Project 2',
+        done: true,
+      }, {
+        title: 'Todo 3',
+        project: 'Project 3',
+        done: false,
+      }, {
+        title: 'Todo 4',
+        project: 'Project 4',
+        done: false,
+      }],
+    };
+  },
+  methods: {
+    createTodo(newTodo) {
+      this.todos.push(newTodo);
+      sweetalert('Success!', 'To-Do created!', 'success');
+    },
+  },
+};
+</script>
